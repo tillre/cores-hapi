e117691492fd0eedda723dee820b12263db2a457
Till Reitemeyer <till@skoni.de>
2014-03-15 17:20:35 +0100

    remove obsolete comments

--- index.js
+++ index.js
@@ -7,17 +7,9 @@ var createApi = require('./lib/create-api.js');
 //
 module.exports.register = function(plugin, options, next) {
 
-  // var selection = plugin;
-  // if (options.selection) {
-  //   selection = plugin.select(options.selection);
-  // }
-
   if (!options.dbUrl) {
     return next(new Error('No database url specified'));
   }
-  // if (!options.resourceDir) {
-  //   return next(new Error('No resource directory specified'));
-  // }
 
   Common.debug = options.debug;
 
@@ -25,11 +17,6 @@ module.exports.register = function(plugin, options, next) {
 
   // make sure db is there
   cores.info().then(function(info) {
-    // return cores.load(options.resourceDir, options.context, options.syncDesign);
-    // }).then(function(resources) {
-    // Object.keys(resources).forEach(function(name) {
-    //   plugin.log(['cores-hapi'], 'loaded resource ' + name);
-    // });
 
     plugin.expose('cores', cores);
     plugin.expose('createApi', function(apiOptions) {
